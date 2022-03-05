284122f7dcb99de9c3ea1e629a16402c39fd9f47
angelgiovanna <aescobar2045@gmail.com>
2022-03-05 11:36:55 -0500

    Final Push

---
+++ assets/script.js
@@ -0,0 +1,70 @@
+var displayCurrentDay = document.querySelector("#currentDay");
+var currentDay = moment();
+displayCurrentDay.textContent = currentDay.format("dddd, MMMM Do YYYY");
+
+
+var timeBlock = $(".time-block").addClass("row");
+var blockText = $("<p>").addClass("description");
+timeBlock.append(blockText);
+
+var currentHour = parseInt(moment().format("H"));
+
+var loadEvents = function (timeSlots) {
+  timeSlots.forEach((element) => {
+    console.log(element);
+    let text = localStorage.getItem(parseInt(element.time));
+    console.log(text);
+    if (text) {
+      element.text.val(text);
+    }
+  });
+};
+
+var fetchEvents = function () {
+  var tempArr = [];
+  $("textarea").each(function (index, elem) {
+    tempArr.push({
+      time: $(elem).attr("id"),
+      text: $(elem),
+    });
+  });
+  loadEvents(tempArr);
+};
+
+$("textarea").each(function () {
+  var $this = $(this);
+  var id = parseInt($this.attr("id"));
+
+  if (id < currentHour) {
+    $(this).addClass("past");
+  }
+  if (id > currentHour) {
+    $(this).addClass("future");
+  }
+  if (id === currentHour) {
+    $(this).addClass("present");
+  }
+});
+
+$("button.saveBtn").click(function (event, loadEvents) {
+  event.preventDefault();
+
+  var $element = $(this).siblings("textarea");
+
+  var time = $element.attr("id");
+  console.log(time);
+  
+  var text = $element.val().trim();
+  console.log(text);
+
+  if (time && text !== "") {
+    console.log(time, text);
+    localStorage.setItem(time, text);
+  }
+});
+
+$(".saveBtn").hover(function () {
+  $(this).addClass("saveBtn:hover");
+});
+
+fetchEvents();
